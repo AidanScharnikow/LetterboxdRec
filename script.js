0e41560a630924bf4944a38b89648b52c88e3813
Aidan Scharnikow <tup95685@temple.edu>
2023-10-04 10:17:00 -0400

    Created simple website showcasing how I will be getting user input from the webstie. This input will be utilized in the future to get the letterboxd user name.

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", function () {
+    const searchInput = document.getElementById("search-input");
+    const searchButton = document.getElementById("search-button");
+    const searchResults = document.getElementById("search-results");
+
+    searchButton.addEventListener("click", function () {
+        const searchTerm = searchInput.value.trim();
+        if (searchTerm !== "") {
+            // Replace this with actual search logic
+            searchResults.innerHTML = `<p>Search results for: <strong>${searchTerm}</strong></p>`;
+        } else {
+            searchResults.innerHTML = "<p>Please enter a search term.</p>";
+        }
+    });
+});
